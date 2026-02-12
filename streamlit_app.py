cc210067333e400faff213c6406cd25dd7c584d7
Ben Clarke <benhgclarke@gmail.com>
2026-02-12 02:05:46 +0000

    style: simplify bar chart color scale to light-to-dark blue

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def show_strategic_dashboard() -> None:
         df_dashboard = generate.generate_dashboard_data()
         fig = px.bar(
             df_dashboard, x="facility", y="value", color="value",
-            color_continuous_scale=[[0, "#808080"], [0.5, "#0066CC"], [1, "#D3D3D3"]],
+            color_continuous_scale=[[0, "#ADD8E6"], [1, "#00008B"]],
             title="Revenue by Facility"
         )
         fig.update_layout(coloraxis_colorbar=dict(title="Value"))
